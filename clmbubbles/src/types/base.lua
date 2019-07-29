ca3de477ee64b5adbc9e11f2eb63c38fc543b627
aatack <alexanderjtatack@gmail.com>
2019-07-29 01:33:30 +0100

    Add recalculate function for bubbles

--- clmbubbles/src/types/base.lua
+++ clmbubbles/src/types/base.lua
@@ -36,14 +36,14 @@ end
 --- Calculate the first derivative of each of the latent
 -- variables as a function of the current latent variables
 -- and the loss resulting from them.
-function CLMBubble:slope(latvars, loss)
+function CLMBubble:slope(latvars, latloss)
   error("slope not yet implemented")
 end
 
 --- Calculate the second derivative of each of the latent
 -- variables as a function of their current values, the
 -- resulting loss, and their first derivatives.
-function CLMBubble:curve(latvers, loss, slope)
+function CLMBubble:curve(latvers, latloss, latslope)
   error("curve not yet implemented")
 end
 
@@ -65,4 +65,16 @@ end
 --- Make a deep copy of the given set of latent variables.
 function CLMBubble:latcopy(source)
   error("latcopy not yet implemented")
+end
+
+--- Recalculate the values of the loss, slope, and curve
+-- based on the current values of the latent variables.
+function CLMBubble:recalculate()
+  local latvars = self.latvars.value
+  local latloss = self:loss(latvars)
+  local latslope = self:slope(latvars, latloss)
+  local latcurve = self:curve(latvars, latloss, latslope)
+  self.latvars.loss = latloss
+  self.latvars.slope = slope
+  self.latvars.curve = curve
 end
