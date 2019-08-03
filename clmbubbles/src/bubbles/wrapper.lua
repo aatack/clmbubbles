a00b0bc3b38e702ca5184c7bba444352cb9e5776
aatack <alexanderjtatack@gmail.com>
2019-08-03 21:30:53 +0100

    Rebase bubbles to prevent floating point errors

--- clmbubbles/src/bubbles/wrapper.lua
+++ clmbubbles/src/bubbles/wrapper.lua
@@ -2,23 +2,33 @@ Wrapper = {}
 Wrapper.__index = Wrapper
 
 --- Wrap a bubble in an interface for convenient use with the game loop.
-function Wrapper:new(bubble)
+function Wrapper:new(bubble, starttime, limit)
   local wrapper = {}
   setmetatable(wrapper, Wrapper)
 
   wrapper.bubble = bubble
-  wrapper.time = 0
+  wrapper.time = starttime or 0
+  wrapper.limit = limit or 1024
 
   return wrapper
 end
 
 --- Advance the wrapper by the given time.
 function Wrapper:advance(deltatime)
+  if self.time > self.limit then
+    local shift = self.limit + self.time
+    self.time = self.time - shift
+
+    local shifted
+    _, shifted = self.bubble:extrapolate(self.time)
+    self.bubble = shifted:rebase(shift)
+  end
   self.time = self.time + deltatime
 end
 
 --- Draw the bubble through the given camera.
 function Wrapper:draw(camera)
+  local todraw
   self.bubble, todraw = self.bubble:extrapolate(self.time)
   todraw:draw(camera)
 end
