b5ad1793a7f091beaf47e19e7ff420c123a66de6
aatack <alexanderjtatack@gmail.com>
2019-08-23 23:44:58 +0100

    Begin rework of the Bubble class

--- clmbubbles/src/bubbles/bubble.lua
+++ clmbubbles/src/bubbles/bubble.lua
@@ -2,78 +2,25 @@ Bubble = {}
 Bubble.__index = Bubble
 
 --- Base class from which any bubble should inherit.
-function Bubble:new(measuredtime)
+function Bubble:new(measuredtime, measuredvariables)
   local bubble = {}
   setmetatable(bubble, Bubble)
 
   bubble.measuredtime = measuredtime
-
-  bubble.discontinuity = {time = nil, result = nil}
+  bubble.measuredvariables = measuredvariables
+  bubble.time = bubble.measuredtime
 
   return bubble
 end
 
---- Calculate and return the time at which this bubble's next
--- discontinuity occurs.  Return `math.huge` if there is no
--- foreseeable discontinuity.
-function Bubble:_time()
-  error("Bubble:_time has not been implemented")
+--- Set the values of the bubble's variables to their values
+-- at the given time.
+function Bubble:settime(time)
+  error("Bubble:settime has not been implemented")
 end
 
---- Calculate and return the state of this bubble immediately
--- after the next discontinuity.  The `measuredtime` field of the
--- result should equal the `discontinuity.time` field of this bubble.
-function Bubble:_result()
-  error("Bubble:_result has not been implemented")
-end
-
---- Assuming the given time is between the measured and discontinuity
--- times, return a new instance of this bubble where its values have
--- been extrapolated to the given time.
-function Bubble:_extrapolate(time)
-  error("Bubble:_extrapolate has not been implemented")
-end
-
---- Return a new bubble whose values are unchanged but whose
--- measured time has been shifted backwards by the given amount.
-function Bubble:rebase(delta)
-  error("Bubble:rebase has not been implemented")
-end
-
---- Get the time at which this bubble's next discontinuity occurs.
-function Bubble:time()
-  if self.discontinuity.time == nil then
-    self.discontinuity.time = self:_time()
-  end
-  return self.discontinuity.time
-end
-
---- Get the state of this bubble immediately after its next
--- discontinuity occurs.
-function Bubble:result()
-  if self.discontinuity.result == nil then
-    self.discontinuity.result = self:_result()
-  end
-  return self.discontinuity.result
-end
-
---- Return two values.  The second is a new instance of this
--- bubble whose fields have been propagated from its measured time
--- to the given time.  The first returned value is the bubble that
--- produced that result; normally, this will be the same as the
--- bubble the function is called on, but if a discontinuity is
--- crossed then a different bubble will be returned.  This
--- function has undefined behaviour if the passed time is less
--- than the bubble's measured time.
-function Bubble:extrapolate(time)
-  if time >= self:time() then
-    return self:result():extrapolate(time)
-  else
-    return self, self:_extrapolate(time)
-  end
-end
-
---- Draw the bubble using the given camera.
+--- Draw the bubble at the current time, through the perspective of
+-- the given camera.
 function Bubble:draw(camera)
-  -- Does nothing by default
+  error("Bubble:draw has not been implemented")
 end
