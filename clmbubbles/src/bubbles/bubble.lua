8642d259b25f752014956b8d9bcb7655ce79346c
aatack <alexanderjtatack@gmail.com>
2020-01-18 00:41:31 +0000

    Add a rough way of subtyping bubbles

--- clmbubbles/src/bubbles/bubble.lua
+++ clmbubbles/src/bubbles/bubble.lua
@@ -1,9 +1,31 @@
 require "util.classes"
 
 Bubble = class()
+Bubble.newevent = {}
 
 --- Create a new bubble, which evolves over time from an initial
 -- state.
 function Bubble:new()
-  error("Bubble:new has not been defined")
+  local bubble = Bubble:init()
+
+  bubble.events.incoming = {}
+  bubble.events.outgoing = {}
+
+  bubble.initial = {}
+  bubble.current = {}
+  bubble.referencetime = 0
+  bubble.currenttime = 0
+
+  return bubble
+end
+
+--- Create a new bubble type, which is a subtype of the bubble type
+-- upon which it is called.
+function Bubble:childclass(updatefunction)
+  local childclass = class(self)
+
+  childclass.updatefunction = updatefunction
+  setmetatable(childclass.newevent, self.newevent)
+
+  return childclass
 end
